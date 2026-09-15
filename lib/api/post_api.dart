8a5630ac5d7906ee9c6392a2ea17f7b438441c9a
Bynna17 <slfs1@aluno.ifal.edu.br>
2026-09-15 12:10:01 -0300

    15/09 - Alterações feitas na builder_selecionar, na criar_page e na postApi (Não mexer nesse código; irei modificá-lo)

--- lib/api/post_api.dart
+++ lib/api/post_api.dart
@@ -21,8 +21,8 @@ class PostsApi {
     return lista;
   }
 
-    Future<int> inserirPost(Post post) async {
-      dio.post('"https://my-json-server.typicode.com/davyfe/fake_api/post',data: {
+    Future<void> inserirPost(Post post) async {
+      dio.post('"https://my-json-server.typicode.com/davyfe/fake_api/post', data: {
         'titulo': post.titulo,
         'autor': post.autor,
         'tempo': post.tempo,
@@ -34,21 +34,26 @@ class PostsApi {
       } );
     }
 
-    Future<int> deletarPost(int id) async {
-      dio.post('https://my-json-server.typicode.com/davyfe/fake_api/post',
-          queryParameters:
-          id: id,
-          where: 'id=?', whereArgs: [id]);
+    Future<void> deletarPost(int id) async {
+      dio.delete('https://my-json-server.typicode.com/davyfe/fake_api/post',
+          queryParameters: {'id': id});
     }
 
     // usado no posts tab na tela perfil
     Future<List<Post>> listarPorAutor(String autor) async {
-      dio.post('https://my-json-server.typicode.com/davyfe/fake_api/post'
-        queryParameters:
-        ,
-    where: 'autor = ?',
-    whereArgs: [autor],
-    );
+     final response =  await dio.get('https://my-json-server.typicode.com/davyfe/fake_api/post');
+
+     List<Post> lista = [];
+     if (response.statusCode == 200) {
+       for (var json in response.data) {
+         Post post = Post.fromJson(json);
+         lista.add(post);
+
+       }
+     }
+     return lista;
+
+    where: 'autor = ?');
     return result.map((json) => Post.fromJson(json)).toList();
     }
 
